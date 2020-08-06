6d2dd845d2a434cbcf4ddd53459af2765ae1e1d4
Dave McPherson <davemcpherson@wochstudios>
2020-08-05 23:58:18 -0400

    Move yahoo commands to a cog Updated yahoo_api to work with latest version Update requirements.txt

---
+++ cogs/misc.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+import discord
+import logging
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+
+class Misc(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="RIP")
+    async def RIP(self, ctx, *args):
+        logger.info("RIP called")
+        if args:
+            await ctx.send(content="Dicks out for {}".format(args[0]), file=discord.File('harambe-rip.jpg', filename='harambe-rip.jpg'))
+        else:
+            await ctx.send(content="Dicks out for Harambe", file=discord.File('harambe-rip.jpg', filename='harambe-rip.jpg'))
