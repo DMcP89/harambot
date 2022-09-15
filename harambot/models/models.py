aef09cf6816f1a568ccef1fb14c59378acd142f2
DMcP89 <davemcpherson@wochstudios.com>
2022-09-15 10:52:39 -0400

    Adding configurations for mysql and postgres dbs

--- harambot/models/models.py
+++ harambot/models/models.py
@@ -5,7 +5,11 @@ from config import settings
 
 
 if settings.guilds_datastore_type == "postgres":
-    database = PostgresqlDatabase()
+    database = PostgresqlDatabase(settings.guild_db,user=settings.guild_db_user, password=settings.guild_db_pass,
+                                    host=settings.guild_db_host, port=settings.guild_db_port)
+elif settings.guilds_datastore_type == "mysql":
+    database = MySQLDatabase(settings.guild_db,user=settings.guild_db_user, password=settings.guild_db_pass,
+                                    host=settings.guild_db_host, port=settings.guild_db_port)
 else:
     database = SqliteDatabase(settings.guilds_datastore_loc)
 
