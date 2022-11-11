e8fc086e73d9a37c37a19948db3885be1dcdee28
DMcP89 <davemcpherson@wochstudios.com>
2022-11-11 13:26:41 -0500

    Consolidating db configs to database_url Removing databasetype enum

--- harambot/database/models.py
+++ harambot/database/models.py
@@ -1,21 +1,11 @@
 from peewee import *
 from playhouse.db_url import connect
 from config import settings
-from database.databasetype import DatabaseType
-
-if settings.guilds_datastore_type == DatabaseType.POSTGRES.value:
-    database = PostgresqlDatabase(settings.guild_db,user=settings.guild_db_user, password=settings.guild_db_pass,
-                                    host=settings.guild_db_host, port=settings.guild_db_port)
-elif settings.guilds_datastore_type == DatabaseType.MYSQL.value:
-    database = MySQLDatabase(settings.guild_db,user=settings.guild_db_user, password=settings.guild_db_pass,
-                                    host=settings.guild_db_host, port=settings.guild_db_port)
-elif settings.guilds_datastore_type == DatabaseType.SQLITE.value:
-    database = SqliteDatabase(settings.guilds_datastore_loc)
-else:
-    database = SqliteDatabase(':memory:')
 
 if settings.database_url:
     database = connect(settings.database_url)
+else:
+    database = SqliteDatabase(':memory:')
 
 class BaseModel(Model):
     class Meta:
