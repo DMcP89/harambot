eaad04f14b48deb4ae756117a69e3264e332a58a
DMcP89 <davemcpherson@wochstudios.com>
2023-01-03 23:04:36 -0500

    Migrating RIP command to slash command

--- harambot/cogs/misc.py
+++ harambot/cogs/misc.py
@@ -1,9 +1,9 @@
-from discord.ext import commands
-
-
 import discord
 import logging
 
+from discord.ext import commands
+from discord import app_commands
+from typing import Optional
 from harambot.database.models import Guild
 
 logger = logging.getLogger(__file__)
@@ -14,13 +14,15 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command("RIP")
-    async def RIP(self, ctx, *args):
+    @app_commands.command(name="rip", description="Pay respects to Harambe")
+    @app_commands.describe(deceased="Who you are paying respects to")
+    async def rip(
+        self, interaction: discord.Interaction, deceased: Optional[str] = None
+    ):
+
         logger.info("RIP called")
-        guild = Guild.get(Guild.guild_id == str(ctx.guild.id))
-        respected = args[0] if args else "Harambe"
-        message = guild.RIP_text + " " + respected
+        guild = Guild.get(Guild.guild_id == str(interaction.guild_id))
+        message = guild.RIP_text + " " + (deceased if deceased else "Harambe")
         embed = discord.Embed(title="", description="", color=0xEEE657)
         embed.set_image(url=guild.RIP_image_url)
-
-        await ctx.send(content=message, embed=embed)
+        await interaction.response.send_message(content=message, embed=embed)
