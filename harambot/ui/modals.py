7fe039c1d804bbf367f1efe6d55fdf8ffcde6cd4
DMcP89 <davemcpherson@wochstudios.com>
2023-01-24 16:08:43 -0500

    Adding required fields to config modal

--- harambot/ui/modals.py
+++ harambot/ui/modals.py
@@ -3,14 +3,31 @@ import discord
 
 class ConfigModal(discord.ui.Modal, title="Configure Guild"):
 
-    name = discord.ui.TextInput(
-        label="Name",
-        placeholder="Your name here...",
+    yahoo_auth_code = discord.ui.TextInput(
+        label="Yahoo Auth Code",
+        placeholder="Enter code from Yahoo Authentication Url",
+    )
+
+    league_id = discord.ui.TextInput(
+        label="Yahoo League ID", placeholder="Enter Yahoo League ID"
+    )
+    leauge_type = discord.ui.TextInput(
+        label="Yahoo League Type",
+        placeholder="Enter Yahoo League Type(nfl, nhl, nba, mlb)",
+    )
+    RIP_text = discord.ui.TextInput(
+        label="RIP command text",
+        placeholder="Enter text to use with $RIP command",
+    )
+    RIP_image_url = discord.ui.TextInput(
+        label="RIP Image",
+        placeholder="Enter image url to use with $RIP command",
     )
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.send_message(
-            f"Thanks for your feedback, {self.name.value}!", ephemeral=True
+            f"Thanks for your feedback, {self.yahoo_auth_code.value}!",
+            ephemeral=True,
         )
 
     async def on_error(
