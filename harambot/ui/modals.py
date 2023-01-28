90124790f564b941161394e57b73e8107c8cf6cd
DMcP89 <davemcpherson@wochstudios.com>
2023-01-27 20:48:15 -0500

    Finished implementing config modal for updating existing guilds

--- harambot/ui/modals.py
+++ harambot/ui/modals.py
@@ -1,5 +1,9 @@
 import discord
 
+from discord.utils import MISSING
+from typing import Optional
+from harambot.database.models import Guild
+
 
 class ConfigModal(discord.ui.Modal, title="Configure Guild"):
 
@@ -19,16 +23,43 @@ class ConfigModal(discord.ui.Modal, title="Configure Guild"):
         placeholder="Enter image url to use with $RIP command",
     )
 
+    guild = None
+
+    def __init__(
+        self,
+        *,
+        title: str = MISSING,
+        timeout: Optional[float] = None,
+        custom_id: str = MISSING,
+        guild: Guild = None,
+    ) -> None:
+        super().__init__(title=title, timeout=timeout, custom_id=custom_id)
+        self.guild = guild
+        self.league_id.default = guild.league_id
+        self.leauge_type.default = guild.league_type
+        self.RIP_text.default = guild.RIP_text
+        self.RIP_image_url.default = guild.RIP_image_url
+
     async def on_submit(self, interaction: discord.Interaction):
+        details = {
+            "league_id": self.league_id.value,
+            "league_type": self.leauge_type.value,
+            "RIP_text": self.RIP_text.value,
+            "RIP_image_url": self.RIP_image_url.value,
+        }
+        Guild.update(details).where(
+            Guild.guild_id == self.guild.guild_id
+        ).execute()
         await interaction.response.send_message(
-            f"Thanks for your feedback, {self.yahoo_auth_code.value}!",
+            "Guild settings updated!",
             ephemeral=True,
         )
 
     async def on_error(
         self, interaction: discord.Interaction, error: Exception
     ) -> None:
-
         await interaction.response.send_message(
-            "Oops! Something went wrong.", ephemeral=True
+            "Oops! Something went wrong with configuring your guild.\
+             Please try again",
+            ephemeral=True,
         )
