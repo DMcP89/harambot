10cab8ee6ef563257cde46d52b636ea9708398cc
DMcP89 <davemcpherson@wochstudios.com>
2023-10-11 22:53:57 -0400

    Removing intents while application is being processed

--- harambot/bot.py
+++ harambot/bot.py
@@ -20,9 +20,9 @@ else:
     logger.setLevel("INFO")
 
 intents = discord.Intents.default()
-intents.members = True
-intents.messages = True
-intents.message_content = True
+# intents.members = True
+# intents.messages = True
+# intents.message_content = True
 
 bot = commands.Bot(command_prefix="$", description="", intents=intents)
 bot.remove_command("help")
@@ -50,10 +50,10 @@ async def on_guild_join(guild):
     logger.info("Joined {}".format(guild.name))
     if not Guild.select().where(Guild.guild_id == str(guild.id)).exists():
         logger.info("Guild not configured!")
-        await guild.owner.send(
-            """Thank you for adding Harambot to your server!
-        Please complete your setup by running the /configure command!"""
-        )
+        # await guild.owner.send(
+        #     """Thank you for adding Harambot to your server!
+        # Please complete your setup by running the /configure command!"""
+        # )
 
 
 def run():
